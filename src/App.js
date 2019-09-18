b5353e21a574ced3b39c0f274ae9cf28de82d11e
fvck3oy <fvck3oy@hotmail.com>
2019-09-18 21:52:56 +0700

    update overview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
 import './App.css';
-
+import Login from './components/Login/Login'
+import Forgotpass from './components/Forgotpass/Forgotpass'
+import Overview from './components/Overview/Overview'
+import  Main from "./Main";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={'z-app/build'}>
+      <Switch>
+        <Route exact path="/" component={withRouter(Login)} />
+        <Route exact path="/forgotpass" component={withRouter(Forgotpass)} />
+        {/* <Route exact path="/register" component={withRouter(Register)} />
+        <Route exact path="/resetpassword/:token" component={withRouter(ResetPassword)} /> */}
+      <Route exact path="/overview" component={withRouter(Overview)} />
+      
+        <Main />
+      </Switch>
+    </Router>
   );
 }
 
